a685198a9b8b679a24de9b16521fe4ccdae1b1a4
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-07-29 16:32:58 +0200

    create Journal page, to be continued...

--- client/src/pages/Journal.jsx
+++ client/src/pages/Journal.jsx
@@ -1,1 +1,13 @@
 // Journaling page
+
+import { useAuth } from "../context/AuthContext"
+import {
+    useEffect,
+    useState
+} from "react"
+
+export default function Journal() {
+    const { user } = useAuth()
+    const [entries, setEntries] = useState([])
+    const [text, setText] = useState("")
+}
