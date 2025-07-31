8d75072b56f9d20ca54c0d425949fb18641b22b2
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-07-31 16:24:56 +0200

    protecting .env file

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,2 +1,22 @@
 // Registration and login handle
 
+import express from "express";
+import { readJSON,
+     writeJSON } from "../utils/fileHandler";
+import path from "path";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"; 
+
+const router = express.Router();
+
+// File path of users.json
+const userFile = path.resolve("server/data/users.json");
+
+// const JWT_SECRET = "use_env_variable";
+
+/**
+ * @route POST /auth/register
+ * @desc REgister new users
+ * @body {email, password}
+ */
+rout
