1ba1d9109f26096b20860f6f1e5fb7a3f6d96116
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-07-31 22:33:46 +0200

    created api.js as a service to centralize api calls

---
+++ client/services/api.js
@@ -0,0 +1,42 @@
+// service to centralize the api calls
+
+const API_URL = "http://localhost:5000";
+
+export async function registerUser(email, password) {
+    const res = await fetch(`${API_URL}/auth/register`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, password}),
+    });
+    
+    return res.json();
+}
+
+export async function loginUser(email, password) {
+    const res = await fetch(`${API_URL}/auth/login`, {
+        method: "POST",
+        headers: {"COntent-Type": "application/json"},
+        body: JSON.stringify({email, password}),
+    });
+
+    return res.json();
+}
+
+export async function getEntries(token) {
+  const res = await fetch(`${API_URL}/journal`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.json();
+}
+
+export async function addEntry(token, text) {
+  const res = await fetch(`${API_URL}/journal`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ text }),
+  });
+  return res.json();
+}
