ca10fea953b59e9fba77ac9bdbd532fb352815d8
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-07-31 18:33:06 +0200

    added POST and DELETE operations to journalRoutes.js

--- server/routes/journalRoutes.js
+++ server/routes/journalRoutes.js
@@ -27,4 +27,59 @@ router.get("/", authMiddleware, async (req, res) => {
     }
 });
 
-// missing POST & DELETE operations...
+/**
+ * @route POST /journal
+ * @desc Aggiunge una nuova entry
+ * @body {text}
+ */
+router.post("/", authMiddleware, async (req, res) => {
+    try {
+        const { text } = req.body;
+
+        if(!text || !text.trim()) {
+            return res.status(400).json({message: "Entry cannot be empty"});
+        }
+
+        const allEntries = await readJSON(entriesFile);
+
+        const newEntry = {
+            id: Date.now(),
+            userId: req.user.id,
+            text,
+            date: new Date().toISOString()
+        };
+
+        allEntries.push(newEntry);
+        await writeJSON(entriesFile);
+
+        res.status(201).json(newEntry);
+    } catch(error) {
+        res.status(500).json({message: "Server error", error: error.message});
+    }
+});
+
+/**
+ * @route DELETE /journal/:id
+ * @desc Elimina una entry specifica dell'utente
+ */
+router.delete("/:id", authMiddleware, async (req, res) => {
+    try {
+        const entryId = parseInt(req.params.id);
+        const allEntries = await readJSON(entriesFile);
+
+        const index = allEntries.findIndex(entry => entry.id === entryId && entry.usedId === req.user.id);
+
+        if(index === -1) {
+            return res.status(404).json({message: "Entry not found or unathorized"});
+        }
+
+        allEntries.splice(index, 1);
+        await writeJSON(entriesFile, allEntries)
+
+        res.json({message: "Entry removed successfully"});
+    } catch(error) {
+        res.status(400).json({message: "Server error", error: error.message});
+    }
+});
+
+export default router;
